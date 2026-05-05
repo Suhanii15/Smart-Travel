3fedd860e43a9524611a1ec381a914ee03b23639
Suhanii15 <suhanikabra931@gmail.com>
2026-05-06 02:28:51 +0530

    added frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+         
+           function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-600">
+      Hoalllaa Amoiggooo
+    </h1>
+  )
+}
+
+
+export default App
