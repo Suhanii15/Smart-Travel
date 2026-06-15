5144bfda0ec7f603c66c526fa65eaad8ee7e28b3
Suhanii15 <suhanikabra931@gmail.com>
2026-06-15 15:09:24 +0530

    removed budget traker from side bar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -40,12 +40,7 @@ const { user, logoutUser } = useContext(AuthContext);
             <Notebook className="text-gray-500" strokeWidth={2.5} />
             Planner </NavLink>
 
-            
-            <NavLink to="/budgettracker" className={ ({isActive})=>` block px-2 py-2 mx-3 shodow-sm flex gap-2 cursor-pointer ${isActive ? 
-          "bg-blue-600 border border-gray-100 rounded-md text-white font-semibold " : " rounded-md shadow-md"}`}  >
-            <HandCoins className="text-gray-500" strokeWidth={2.5} />
-            Budget Tracker </NavLink>
-
+          
         </ul>
 
 <div 
